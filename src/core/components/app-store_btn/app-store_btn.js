292bd458c84bbe1daeffecfaf1dd8ddf7dbbe39b
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2023-11-25 10:48:41 +0100

    feat: create mobile App block

---
+++ src/core/components/app-store_btn/app-store_btn.js
@@ -0,0 +1,28 @@
+import createElement from '../../utils/createElement';
+import Component from '../template/component';
+
+class AppStoreBtn extends Component {
+  constructor(logoHTML, text, storeText) {
+    super('button', 'app-Store-Btn');
+    this.logoWrap.innerHTML = logoHTML;
+    this.text.innerHTML = text;
+    this.storeText.innerHTML = storeText;
+  }
+
+  createElements() {
+    this.logoWrap = createElement({ tagName: 'div', className: 'logo-wrap' });
+    this.textContainer = createElement({ tagName: 'div', className: 'text-container' });
+    this.text = createElement({ tagName: 'p' });
+    this.storeText = createElement({ tagName: 'p' });
+  }
+
+  appendElements() {
+    this.textContainer.append(this.text);
+    this.textContainer.append(this.storeText);
+
+    this.container.append(this.logoWrap);
+    this.container.append(this.textContainer);
+  }
+}
+
+export default AppStoreBtn;
--- src/core/pages/home/home.js
+++ src/core/pages/home/home.js
@@ -1,6 +1,7 @@
 import AboutSection from '../../components/layout/about_section/about_section';
 import Enjoy from '../../components/layout/enjoy_block/enjoy';
 import FavoritesCoffee from '../../components/layout/favorites_coffee/favorites_coffee';
+import MobileApp from '../../components/layout/mobile-app_block/mobile-app_block';
 import Component from '../../components/template/component';
 
 class HomePage extends Component {
@@ -12,6 +13,7 @@ class HomePage extends Component {
     this.container.append(new Enjoy().getElement());
     this.container.append(new FavoritesCoffee().getElement());
     this.container.append(new AboutSection().getElement());
+    this.container.append(new MobileApp().getElement());
   }
 }
 
